aec98a7e1e1bc78a3a606238b2780ac9580c88d2
MatheusKCarvalho <131721822+MatheusKCarvalho@users.noreply.github.com>
2026-04-02 23:58:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function trocarTema() {
     document.body.classList.toggle("escuro")
     }
+
+function addTarefa() {
+        let input = document.getElementById("novaTarefa");
+        let lista = document.getElementById("lista");
+
+        let texto = input.value
+
+        lista.innerText = texto
+
+}
